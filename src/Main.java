1c2192d48ce393728d391f07d29bcbf480f915a9
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2025-10-17 17:15:04 -0700

    Finished time complexity for hasCommon() method

--- src/Main.java
+++ src/Main.java
@@ -185,7 +185,7 @@ public class Main {
   // What is the time complexity of this method?
   // assume that each String is bounded by a constant length
   // (assume the set and list have the same number of elements)
-  // YOUR ANSWER HERE
+  // YOUR ANSWER HERE: O(n^2)
   public static boolean hasCommon(HashSet<String> wordSet, ArrayList<String> wordList) {
     for(String word : wordSet) {
       if(wordList.contains(word)) {
